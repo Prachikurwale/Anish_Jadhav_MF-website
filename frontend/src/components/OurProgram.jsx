9fb336f689bb57f551c2d7c676964332dc5a38ee
Renuka <renukachouhan24@navgurukul.org>
2025-11-06 12:58:30 +0530

    Resolve Eslint error

--- frontend/src/components/OurProgram.jsx
+++ frontend/src/components/OurProgram.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion' // eslint-disable-line no-unused-vars
 import { CheckCircle, Code, Briefcase, GraduationCap, RefreshCw, ChevronRight } from 'lucide-react'
 import styles from './OurPrograms.module.css'
 
